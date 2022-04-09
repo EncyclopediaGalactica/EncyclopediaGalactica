b7eac11921342804a6a0a39d8197e3a20fd7e12a
Andras Csanyi <andras.csanyi@pm.me>
2022-04-09 12:56:48 +0200

    parallel test exec fix

---
+++ SourceFormats/Repository.Int.Tests/SourceFormatNode/RepositoryCollectionDefinition.cs
@@ -0,0 +1,10 @@
+namespace EncyclopediaGalactica.SourceFormats.Repository.Int.Tests.SourceFormatNode;
+
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+[ExcludeFromCodeCoverage]
+[CollectionDefinition("SourceFormatNode Repository Collection", DisableParallelization = true)]
+public class RepositoryCollectionDefinition
+{
+}
