9c2c8d6eaa9c8f96bc6670a6d65eeb9529918a6c
Andras Csanyi <andras.csanyi@pm.me>
2022-06-05 12:14:29 +0200

    fixes

--- Host/RestApi/Program.cs
+++ Host/RestApi/Program.cs
@@ -29,7 +29,6 @@ builder.Services
     .AddApplicationPart(typeof(SourceFormatNodeController).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ISourceFormatMappers, SourceFormatMappers>();
 builder.Services.AddScoped<ISourceFormatNodeMappers, SourceFormatNodeMappers>();
@@ -72,11 +71,11 @@ using (IServiceScope scope = sp.CreateScope())
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
