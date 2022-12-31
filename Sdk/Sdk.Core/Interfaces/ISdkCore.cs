c061ed2595357a62bad1dd75d1283055a707dd54
Andras Csanyi <andras.csanyi@pm.me>
2022-12-31 12:42:33 +0100

    some change

--- Sdk/Sdk.Core/Interfaces/ISdkCore.cs
+++ Sdk/Sdk.Core/Interfaces/ISdkCore.cs
@@ -2,6 +2,9 @@
 
 using Model.Interfaces;
 
+/// <summary>
+/// The Sdk Core Interface
+/// </summary>
 public interface ISdkCore
 {
     Task<TResponseModel> SendAsync<TResponseModel, TResponseModelPayload>(
