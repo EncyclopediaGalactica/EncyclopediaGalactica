0c89dd9ad01c2ef802bf86cc3398cf962cca8c58
András Csányi <andras.csanyi@pm.me>
2023-11-03 09:40:22 +0100

    feat(IAM): display modules in the right-top dropdown (#131)
    This PR brings the following changes:
    
    * IAM Modules are displayed on the right-top corner in a dropdown

--- src/main/java/com/andrascsanyi/encyclopediagalactica/ui/infra/controllers/IndexController.java
+++ src/main/java/com/andrascsanyi/encyclopediagalactica/ui/infra/controllers/IndexController.java
@@ -1,5 +1,11 @@
 package com.andrascsanyi.encyclopediagalactica.ui.infra.controllers;
 
+import com.andrascsanyi.encyclopediagalactica.iam.application.GetModulesScenario;
+import com.andrascsanyi.encyclopediagalactica.iam.contracts.ModuleOutput;
+import java.util.List;
+import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +15,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/")
 public class IndexController {
 
-    @GetMapping
-    public String getMainPage(Model model) {
+    private final GetModulesScenario getModulesScenario;
+    private final static Logger log = LoggerFactory.getLogger(IndexController.class);
 
+    public IndexController(
+        @NonNull GetModulesScenario getModulesScenario) {
+        this.getModulesScenario = getModulesScenario;
+    }
+
+    @GetMapping("/")
+    public String getMainPage(Model model) {
+        String message = IndexController.class.getName() + "getMainPage() is called.";
+        log.info(message);
         return "index.html";
+    }
+
+    @GetMapping("/fragments/header")
+    public String getPageHeader(Model model) {
+        
+        String message = IndexController.class.getName() + "getPAgeHeader() is called.";
+        log.info(message);
+
+        List<ModuleOutput> modules = getModulesScenario.getModules();
+        model.addAttribute("moduleList", modules);
+        return "fragments/header.html";
     }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO module(name, description) VALUES ('finance', 'finance module');
+INSERT INTO module(name, description) VALUES ('health', 'health module');
+INSERT INTO module(name, description) VALUES ('thesaurus', 'thesaurus module');
