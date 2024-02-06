0567453b760bd0ef7d0ee17622224e44a3a02172
Andras Csanyi <andras.csanyi@pm.me>
2024-02-06 14:21:59 +0100

    fix

--- src/java/src/test/java/com/encyclopediagalactica/document/DocumentTestUtils.java
+++ src/java/src/test/java/com/encyclopediagalactica/document/DocumentTestUtils.java
@@ -1,39 +1,17 @@
 package com.encyclopediagalactica.document;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.encyclopediagalactica.api.graphql.Document;
 import com.encyclopediagalactica.document.model.DocumentEntity;
 
-@Service
-public class DocumentTestUtils {
-
-    public Iterable<DocumentEntity> createDocumentEntities(int amount) {
-        List<DocumentEntity> result = new ArrayList<>();
-
-        for (int i = 0; i < amount; i++) {
-            result.add(DocumentEntity.builder().id((long) i).name("name" + i).desc("desc" + i)
-                    .build());
-        }
-        return result.stream().toList();
-    }
+public interface DocumentTestUtils {
+    Iterable<DocumentEntity> createDocumentEntities(int amount, Boolean isIdZero);
 
     /**
-     * Creates the provided amount of unique {@link Document} entities.
+     * Creates the given amount {@link Document} object.
      *
-     * @param amount the amount of entities will be created
-     * @return {@link List<Document>} result
+     * @param amount   the amount
+     * @param isIdZero if true Id value will be zero at every item
+     * @return {@link java.util.List<Document>}
      */
-    public Iterable<Document> createDocuments(int amount) {
-        List<Document> result = new ArrayList<>();
-
-        for (int i = 0; i < amount; i++) {
-            result.add(Document.builder().setId("100" + i).setName("name " + i).setDesc("desc " + i)
-                    .build());
-        }
-        return result.stream().toList();
-    }
+    Iterable<Document> createDocuments(int amount, Boolean isIdZero);
 }
