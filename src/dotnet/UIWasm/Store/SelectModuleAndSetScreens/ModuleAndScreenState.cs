57982370c0c3c9d7b84c502254007cb5bf6975e6
Andras Csanyi <andras.csanyi@pm.me>
2024-04-04 22:12:00 +0200

    screens

--- src/dotnet/UIWasm/Store/SelectModuleAndSetScreens/ModuleAndScreenState.cs
+++ src/dotnet/UIWasm/Store/SelectModuleAndSetScreens/ModuleAndScreenState.cs
@@ -4,20 +4,26 @@ using EncyclopediaGalactica.BusinessLogic.Contracts;
 using Fluxor;
 
 [FeatureState]
-public class ModuleState
+public class ModuleAndScreenState
 {
-    private ModuleState()
+    private ModuleAndScreenState()
     {
     }
 
-    public ModuleState(ModuleResult? selectedModule, IEnumerable<ScreenResult>? screensOfModule, bool isLoading)
+    public ModuleAndScreenState(
+        ModuleResult? selectedModule,
+        IEnumerable<ScreenResult>? screensOfModule,
+        string? selectedScreen,
+        bool isLoading)
     {
         SelectedModule = selectedModule;
         ScreensOfModule = screensOfModule;
+        SelectedScreen = selectedScreen;
         IsLoading = isLoading;
     }
 
     public ModuleResult? SelectedModule { get; }
+    public string? SelectedScreen { get; set; }
     public bool IsLoading { get; }
     public IEnumerable<ScreenResult>? ScreensOfModule { get; }
 }
