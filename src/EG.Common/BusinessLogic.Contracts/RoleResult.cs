a14fbdd115aa8f6747780277ddce0509a399e7dc
Andras Csanyi <andras.csanyi@pm.me>
2024-04-13 18:15:59 +0200

    fix

--- src/EG.Common/BusinessLogic.Contracts/RoleResult.cs
+++ src/EG.Common/BusinessLogic.Contracts/RoleResult.cs
@@ -3,6 +3,8 @@ namespace EncyclopediaGalactica.BusinessLogic.Contracts;
 public class RoleResult
 {
     public long Id { get; set; }
+    
     public string Name { get; set; }
+    
     public string Description { get; set; }
 }
