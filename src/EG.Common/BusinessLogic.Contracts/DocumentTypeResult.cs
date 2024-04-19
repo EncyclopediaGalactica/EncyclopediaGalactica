50d8699bfc77a4936d73bec7f19270fdef913680
András Csányi <andras.csanyi@pm.me>
2024-04-19 15:06:17 +0200

    feat: Document Type Result #234

---
+++ src/EG.Common/BusinessLogic.Contracts/DocumentTypeResult.cs
@@ -0,0 +1,8 @@
+namespace EncyclopediaGalactica.BusinessLogic.Contracts;
+
+public class DocumentTypeResult
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
