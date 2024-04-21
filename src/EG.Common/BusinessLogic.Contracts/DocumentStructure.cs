60fcf63050d3ba0b7ea2d05237da13f8000d17f2
András Csányi <andras.csanyi@pm.me>
2024-04-21 15:24:07 +0200

    feat: Adding DocumentStructure contract #236

---
+++ src/EG.Common/BusinessLogic.Contracts/DocumentStructure.cs
@@ -0,0 +1,8 @@
+namespace EncyclopediaGalactica.BusinessLogic.Contracts;
+
+public class DocumentStructure
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
