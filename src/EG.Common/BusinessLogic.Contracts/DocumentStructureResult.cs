bfcbbef6915aa404b34c32d775ff679664703ffb
Andras Csanyi <andras.csanyi@pm.me>
2024-04-21 15:30:59 +0200

    fixing name

--- src/EG.Common/BusinessLogic.Contracts/DocumentStructureResult.cs
+++ src/EG.Common/BusinessLogic.Contracts/DocumentStructureResult.cs
@@ -1,6 +1,6 @@
 namespace EncyclopediaGalactica.BusinessLogic.Contracts;
 
-public class DocumentStructure
+public class DocumentStructureResult
 {
     public long Id { get; set; }
     public string Name { get; set; }
