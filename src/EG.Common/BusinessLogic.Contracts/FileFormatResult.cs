822278ad3a434db0cc0a39bd4244c6258b228139
András Csányi <andras.csanyi@pm.me>
2024-04-24 17:35:54 +0200

    feat: add FileFormatResult #239

---
+++ src/EG.Common/BusinessLogic.Contracts/FileFormatResult.cs
@@ -0,0 +1,9 @@
+namespace EncyclopediaGalactica.BusinessLogic.Contracts;
+
+public class FileFormatResult
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string FileExtension { get; set; }
+}
