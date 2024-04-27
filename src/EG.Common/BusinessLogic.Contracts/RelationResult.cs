e50414fb6ead979aad95e5819fe49e4b983e3cca
Andras Csanyi <andras.csanyi@pm.me>
2024-04-27 11:01:56 +0200

    relation is extended by relation type

--- src/EG.Common/BusinessLogic.Contracts/RelationResult.cs
+++ src/EG.Common/BusinessLogic.Contracts/RelationResult.cs
@@ -5,4 +5,5 @@ public class RelationResult
     public long Id { get; set; }
     public DocumentResult LeftDocument { get; set; }
     public DocumentResult RightDocument { get; set; }
+    public RelationTypeResult RelationType { get; set; }
 }
